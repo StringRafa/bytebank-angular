b55b2e7cd39e497a80c5ca37b40e7555f4f2dc19
Rafael <rafa.jo.souza@hotmail.com>
2022-04-04 08:22:09 -0300

    Interpolando dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-nova-transferencia',
@@ -7,7 +7,20 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent{
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = null;
+  destino: number = null;
+
   transferir(){
     console.log('Solicitado nova transferencia');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = null;
+    this.destino = null;
   }
 }
